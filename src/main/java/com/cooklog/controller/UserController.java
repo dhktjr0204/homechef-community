1f10c7e6bf37862330587238898907df3a1bbda7
seoyeon1123 <woghks072@gmail.com>
2024-03-25 22:30:52 +0900

    feat : 이메일 & 비밀번호 validation 검사

--- src/main/java/com/cooklog/controller/UserController.java
+++ src/main/java/com/cooklog/controller/UserController.java
@@ -1,20 +1,26 @@
 package com.cooklog.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cooklog.dto.UserDTO;
 import com.cooklog.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
-	private UserService userService;
+    private final UserService userService;
 
-	@GetMapping("/user/profile")
+    	@GetMapping("/user/profile")
 	public String userProfile(Model model) {
 		// 예시로 1번 ID 사용자 정보를 조회
 		UserDTO userDto = userService.findUserById(1L);
@@ -22,6 +28,16 @@ public class UserController {
 		if (userDto != null) {
 			model.addAttribute("user", userDto);
 		}
-		return "index";
+		return "main/index";
 	}
+
+
+    @PostMapping("/signin/signup")
+    @ResponseBody
+    public ResponseEntity<String> signup(@RequestBody @Valid UserDTO userDTO) {
+        userService.join(userDTO);
+        return ResponseEntity.ok("OK");
+    }
+
+
 }
--- src/main/java/com/cooklog/dto/UserDTO.java
+++ src/main/java/com/cooklog/dto/UserDTO.java
@@ -2,17 +2,25 @@ package com.cooklog.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class UserDTO {
 	private Long idx;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
 	private String nickname;
+	@Email(message = "유효한 이메일 형식이 아닙니다.")
 	private String email;
+	@NotBlank(message = "비밀번호를 입력해주세요.")
 	private String password;
-	private String introduction;
-	private String role;
-	private String profileImage;
-	private Boolean isDeleted;
+
+
+//	private String introduction; //마이페이지
+//	private String role; //마이페이지
+//	private String profileImage; //마이페이지
+//	private Boolean isDeleted; //마이페이지
 
 }
