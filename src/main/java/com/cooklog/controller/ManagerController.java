b045bd7a4bd730d7869916a3b979216e5d75c2a9
HuiGyun-kim <khg940904@gmail.com>
2024-03-26 09:13:27 +0900

    Feat: 관리자 페이지 추가 1.관리자 페이지 추가 2.관리자 - 게시글 관리 페이지 추가

--- src/main/java/com/cooklog/controller/ManagerController.java
+++ src/main/java/com/cooklog/controller/ManagerController.java
@@ -1,21 +1,23 @@
 package com.cooklog.controller;
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cooklog.dto.UserDTO;
 import com.cooklog.service.UserService;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequiredArgsConstructor
-public class UserController {
+@RequestMapping("/manager")
+public class ManagerController {
 
 	private final UserService userService;
 
-	@GetMapping("/user/profile")
+	@GetMapping("/main")
 	public String userProfile(Model model) {
 		// 예시로 1번 ID 사용자 정보를 조회
 		UserDTO userDto = userService.findUserById(1L);
@@ -23,6 +25,21 @@ public class UserController {
 		if (userDto != null) {
 			model.addAttribute("user", userDto);
 		}
-		return "main/index";
+		return "manager/manager";
+	}
+
+	@GetMapping("/board")
+	public String board() {
+		return "manager/board-manager";
+	}
+
+	@GetMapping("/comment")
+	public String command() {
+		return "manager/comment-manager";
+	}
+
+	@GetMapping("/report")
+	public String report() {
+		return "manager/report-manager";
 	}
 }
