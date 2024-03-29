027a8a1b3711515cf073511215719bd545066bf6
dhktjr0204 <dhktjr0204442@gmail.com>
2024-03-29 17:38:19 +0900

    Feat: 검색 기능 추가

--- src/main/java/com/cooklog/model/Image.java
+++ src/main/java/com/cooklog/model/Image.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
--- src/main/resources/static/js/main/search.js
+++ src/main/resources/static/js/main/search.js
@@ -1,8 +1,28 @@
 function submitForm() {
-    let keyword = document.querySelector(".searchInput").value.trim();
+    let keyword = document.querySelector(".search-input").value.trim();
+
     if (keyword === "") {
+        alert("검색어를 입력해주세요");
         return false; // 빈 키워드일 경우 폼 제출 방지
-    } else {
+    } else if(keyword.startsWith("#")){
+        const tagsWithHash = keyword.split(" ");
+        //연속으로 #이 있어도 지울 수 있다.
+        const tags=tagsWithHash
+            .map(tag=> tag.replace(/^#+/,""))
+            .map(tag=>tag.trim())
+            //아무것도 없는 값 제거
+            .filter((tag)=>tag);
+
+        if(tags.length===0){
+            alert("해시태그를 입력해주세요.");
+            return false;
+        }
+
+        location.href=`/hashtag_search?keyword=${tags.join(",")}`;
+
+        return false;
+    }
+    else {
         return true; // 검색어가 있을 경우 폼 제출
     }
 }
